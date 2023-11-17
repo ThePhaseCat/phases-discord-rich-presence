f7cd90c8b990fec19e3d56953009d5eef53c3e76
ThePhaseCat <alexmgamer10@gmail.com>
2023-11-16 17:50:41 -0700

    it broken

--- src/client/java/phasesdiscordConfigStuff/phasesdiscordConfig.java
+++ src/client/java/phasesdiscordConfigStuff/phasesdiscordConfig.java
@@ -2,13 +2,13 @@ package phasesdiscordConfigStuff;
 
 import eu.midnightdust.lib.config.MidnightConfig;
 
-public class phasesdiscordConfig extends MidnightConfig {
+public class phasesdiscordConfig extends MidnightConfig
+{
+    @Entry public static boolean discordEnable = true;
 
-    @Entry(category = "basic") public static boolean discordEnable = true;
+    @Entry public static boolean enableServerIP = true;
 
-    @Entry(category = "basic") public static boolean enableServerIP = true;
+    @Entry public static boolean enableItem = true;
 
-    @Entry(category = "basic") public static boolean enableItem = true;
-
-    @Entry(category = "basic") public static boolean enableDimension = true;
+    @Entry public static boolean enableDimension = true;
 }
