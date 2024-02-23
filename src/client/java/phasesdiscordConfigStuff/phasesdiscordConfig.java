06330c6a8844ca20d003a4a9a17c1adbae9facf5
ThePhaseCat <alexmgamer10@gmail.com>
2024-02-23 09:46:55 -0700

    added a bunch of new advanced mode variables
    -basically the title, still have a few more to do
    -just options at the moment, changing them does nothing yet

--- src/client/java/phasesdiscordConfigStuff/phasesdiscordConfig.java
+++ src/client/java/phasesdiscordConfigStuff/phasesdiscordConfig.java
@@ -19,4 +19,15 @@ public class phasesdiscordConfig extends MidnightConfig
 
     @Comment(centered = true) public static Comment text2;
     @Entry public static boolean enableAdvancedMode = false;
+
+    @Comment public static Comment text3;
+    @Comment public static Comment text4;
+    @Entry public static String mainAdvancedModeDetail = "Playing Minecraft";
+    @Entry public static String mainAdvancedModeDetailWhenHoldingItem = "Holding {item_name}";
+
+    @Entry public static String mainAdvancedModeStateMultiplayer = "Playing multiplayer on {server_ip}";
+    @Entry public static String mainAdvancedModeStateMultiplayerPause = "Playing multiplayer on {server_ip} - Paused";
+
+    @Entry public static String mainAdvancedModeStateSingleplayer = "Playing Singleplayer";
+    @Entry public static String mainAdvancedModeStateSingleplayerPause = "Playing Singleplayer - Paused";
 }
