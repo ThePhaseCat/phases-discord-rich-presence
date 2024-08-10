bf6012ff5cf45e4bc1387bdc6dd82ceb6c277c82
ThePhaseCat <alexmgamer10@gmail.com>
2024-08-10 11:01:14 -0600

    1.20.4 branch setup

--- src/client/java/com/phasesdiscord/MenuIntegration.java
+++ src/client/java/com/phasesdiscord/MenuIntegration.java
@@ -5,15 +5,7 @@ import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import eu.midnightdust.lib.config.MidnightConfig;
 
-/**
- * ModMenu integration class
- */
 public class MenuIntegration implements ModMenuApi {
-
-    /**
-     * Get the config screen factory
-     * @return the config screen factory
-     */
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return parent -> MidnightConfig.getScreen(parent, "phases-discord-rich-presence");
