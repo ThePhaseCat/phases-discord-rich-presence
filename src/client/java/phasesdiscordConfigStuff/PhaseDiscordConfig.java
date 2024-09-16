bd5fb8a135492d06697b896fa592181f30a4ed50
ThePhaseCat <84151006+ThePhaseCat@users.noreply.github.com>
2024-09-16 15:31:07 -0600

    added the other categories

--- src/client/java/phasesdiscordConfigStuff/PhaseDiscordConfig.java
+++ src/client/java/phasesdiscordConfigStuff/PhaseDiscordConfig.java
@@ -5,6 +5,8 @@ import eu.midnightdust.lib.config.MidnightConfig;
 public class PhaseDiscordConfig extends MidnightConfig
 {
     public static final String BASIC = "basic";
+    public static final String ADVANCED = "advanced";
+    public static final String OTHER = "other";
 
     @Comment(category = BASIC, centered = true) public static Comment text1;
     @Entry(category = BASIC) public static boolean discordEnable = true;
@@ -19,58 +21,58 @@ public class PhaseDiscordConfig extends MidnightConfig
 
     @Entry(category = BASIC) public static boolean showPaused = true;
 
-    @Comment(centered = true) public static Comment text2;
-    @Entry public static boolean enableAdvancedMode = false;
+    @Comment(category = ADVANCED, centered = true) public static Comment text2;
+    @Entry(category = ADVANCED) public static boolean enableAdvancedMode = false;
 
-    @Comment public static Comment text3;
-    @Comment public static Comment text4;
-    @Comment public static Comment text5;
-    @Comment public static Comment text6;
-    @Entry public static String mainAdvancedModeDetail = "Playing Minecraft";
-    @Entry public static String mainAdvancedModeDetailWhenHoldingItem = "Holding {item_name}";
+    @Comment(category = ADVANCED) public static Comment text3;
+    @Comment(category = ADVANCED) public static Comment text4;
+    @Comment(category = ADVANCED) public static Comment text5;
+    @Comment(category = ADVANCED) public static Comment text6;
+    @Entry(category = ADVANCED) public static String mainAdvancedModeDetail = "Playing Minecraft";
+    @Entry(category = ADVANCED) public static String mainAdvancedModeDetailWhenHoldingItem = "Holding {item_name}";
 
-    @Entry public static String mainAdvancedModeStateMultiplayer = "Playing multiplayer on {server_ip}";
-    @Entry public static String mainAdvancedModeStateMultiplayerPause = "Playing multiplayer on {server_ip} - Paused";
+    @Entry(category = ADVANCED) public static String mainAdvancedModeStateMultiplayer = "Playing multiplayer on {server_ip}";
+    @Entry(category = ADVANCED) public static String mainAdvancedModeStateMultiplayerPause = "Playing multiplayer on {server_ip} - Paused";
 
-    @Entry public static String mainAdvancedModeStateSingleplayer = "Playing Singleplayer";
-    @Entry public static String mainAdvancedModeStateSingleplayerPause = "Playing Singleplayer - Paused";
+    @Entry(category = ADVANCED) public static String mainAdvancedModeStateSingleplayer = "Playing Singleplayer";
+    @Entry(category = ADVANCED) public static String mainAdvancedModeStateSingleplayerPause = "Playing Singleplayer - Paused";
 
-    @Entry public static String advancedModeDimensionOverworld = "In The Overworld";
-    @Entry public static String advancedModeDimensionNether = "In The Nether";
-    @Entry public static String advancedModeDimensionEnd = "In The End";
-    @Entry public static String advancedModeDimensionCustom = "In {dimension_name} Dimension";
+    @Entry(category = ADVANCED) public static String advancedModeDimensionOverworld = "In The Overworld";
+    @Entry(category = ADVANCED) public static String advancedModeDimensionNether = "In The Nether";
+    @Entry(category = ADVANCED) public static String advancedModeDimensionEnd = "In The End";
+    @Entry(category = ADVANCED) public static String advancedModeDimensionCustom = "In {dimension_name} Dimension";
 
-    @Entry public static boolean advancedModeChangeMainMenuText = false;
+    @Entry(category = ADVANCED) public static boolean advancedModeChangeMainMenuText = false;
 
-    @Entry public static String advancedModeMainMenuText = "Main Menu";
+    @Entry(category = ADVANCED) public static String advancedModeMainMenuText = "Main Menu";
 
-    @Comment public static Comment advancedModePicDetail1;
+    @Comment(category = ADVANCED) public static Comment advancedModePicDetail1;
 
-    @Comment public static Comment advancedModePicDetail2;
+    @Comment(category = ADVANCED) public static Comment advancedModePicDetail2;
 
-    @Comment public static Comment advancedModeOverworldPicDetail;
+    @Comment(category = ADVANCED) public static Comment advancedModeOverworldPicDetail;
 
-    @Comment public static Comment advancedModeNetherPicDetail;
+    @Comment(category = ADVANCED) public static Comment advancedModeNetherPicDetail;
 
-    @Comment public static Comment advancedModeEndPicDetail;
+    @Comment(category = ADVANCED) public static Comment advancedModeEndPicDetail;
 
-    @Comment public static Comment advancedModeOtherPicDetail;
+    @Comment(category = ADVANCED) public static Comment advancedModeOtherPicDetail;
 
-    @Entry public static String advancedModeOverworldPic = "overworld";
+    @Entry(category = ADVANCED) public static String advancedModeOverworldPic = "overworld";
 
-    @Entry public static String advancedModeNetherPic = "nether";
+    @Entry(category = ADVANCED) public static String advancedModeNetherPic = "nether";
 
-    @Entry public static String advancedModeEndPic = "the_end";
+    @Entry(category = ADVANCED) public static String advancedModeEndPic = "the_end";
 
-    @Entry public static String advancedModeCustomPic = "void";
+    @Entry(category = ADVANCED) public static String advancedModeCustomPic = "void";
 
-    @Entry public static String advancedModeLargePic = "base";
+    @Entry(category = ADVANCED) public static String advancedModeLargePic = "base";
 
-    @Comment(centered = true) public static Comment otherOptions;
+    @Comment(category = OTHER, centered = true) public static Comment otherOptions;
 
-    @Entry public static boolean enableDebug = false;
+    @Entry(category = OTHER) public static boolean enableDebug = false;
     
-    @Entry public static String discordAppID = "1147361100929708053";
+    @Entry(category = OTHER) public static String discordAppID = "1147361100929708053";
 
-    @Entry public static int discordRichPresenceUpdateRate = 5000;
+    @Entry(category = OTHER) public static int discordRichPresenceUpdateRate = 5000;
 }
