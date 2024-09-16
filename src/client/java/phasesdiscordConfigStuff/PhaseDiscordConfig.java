d6128b6d50adad9c9ff4e686a1ea9db4f17b9982
ThePhaseCat <84151006+ThePhaseCat@users.noreply.github.com>
2024-09-16 15:26:56 -0600

    category start and bumps
    -added a basic category, will expand to all options
    -bumped midnight lib version to 1.6.3 (latest version)

--- src/client/java/phasesdiscordConfigStuff/PhaseDiscordConfig.java
+++ src/client/java/phasesdiscordConfigStuff/PhaseDiscordConfig.java
@@ -4,18 +4,20 @@ import eu.midnightdust.lib.config.MidnightConfig;
 
 public class PhaseDiscordConfig extends MidnightConfig
 {
-    @Comment(centered = true) public static Comment text1;
-    @Entry public static boolean discordEnable = true;
+    public static final String BASIC = "basic";
 
-    @Entry public static boolean enableItem = true;
+    @Comment(category = BASIC, centered = true) public static Comment text1;
+    @Entry(category = BASIC) public static boolean discordEnable = true;
 
-    @Entry public static boolean enableDimension = true;
+    @Entry(category = BASIC) public static boolean enableItem = true;
 
-    @Entry public static boolean enableServerIP = true;
+    @Entry(category = BASIC) public static boolean enableDimension = true;
 
-    @Entry public static boolean enableCustomDimensionSupport = true;
+    @Entry(category = BASIC) public static boolean enableServerIP = true;
 
-    @Entry public static boolean showPaused = true;
+    @Entry(category = BASIC) public static boolean enableCustomDimensionSupport = true;
+
+    @Entry(category = BASIC) public static boolean showPaused = true;
 
     @Comment(centered = true) public static Comment text2;
     @Entry public static boolean enableAdvancedMode = false;
