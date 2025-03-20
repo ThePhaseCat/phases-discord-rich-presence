821182e646cc4e5c13dd37d3161520d3d835d804
ThePhaseCat <84151006+ThePhaseCat@users.noreply.github.com>
2025-03-20 14:05:24 -0600

    bumped some stuff and some quick format stuff

--- src/main/java/com/phasesdiscord/PhaseDiscord.java
+++ src/main/java/com/phasesdiscord/PhaseDiscord.java
@@ -8,8 +8,6 @@ import org.slf4j.LoggerFactory;
 public class PhaseDiscord implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("phases-discord-rich-presence");
 
-
-
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Phases Discord Rich Presence is initializing...");
