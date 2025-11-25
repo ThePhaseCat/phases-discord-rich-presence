2ca0e3c140ecb77524948df563856dce3b9fd8f8
ThePhaseCat <84151006+ThePhaseCat@users.noreply.github.com>
2025-11-25 13:11:51 -0700

    added four new images to advanced mode

--- src/client/java/com/phasesdiscord/RPC.java
+++ src/client/java/com/phasesdiscord/RPC.java
@@ -44,6 +44,7 @@ public class RPC
             "spring1", "spring2", "vibrant_visuals1", "vibrant_visuals2",
             "lush_cave", "dripstone_cave", "deep_dark1", "deep_dark2", "happy_ghast1",
             "coppergolem1", "coppergolem2", "coppergolem3", "coppergolem4",
+            "nautilus1", "nautilus2", "nautilus3", "spear1",
             "nether", "nether2", "nether3", "nethercool",
             "the_end", "end2", "end3", "actualendbg",
             "void", "base", "base_old", "creeper_icon", "fallback", "pack"
